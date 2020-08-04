ec3eec4ee393dd7e997bd6a0ba558fa7683981a2
pradeepkumaryadavre <67374531+pradeepkumaryadavre@users.noreply.github.com>
2020-08-04 19:24:08 +0530

    removed console command

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-//const express = require("express")
-
-console.log('Client side javascript file is loaded')
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
